46862840655eb2834021e1f71caa9adc18a18fcc
UO264451 <UO264451@uniovi.es>
2020-02-27 22:47:45 +0100

    Second version of friends page

---
+++ src/containers/Friends/SubFriends.js
@@ -0,0 +1,97 @@
+import React from "react";
+import data from "@solid/query-ldflex";
+import { useWebId } from "@inrupt/solid-react-components";
+import {NavBar} from "../../components";
+import { Value, List } from '@solid/react';
+
+class SubFriendsComponent extends React.Component {
+  
+    state = {
+        ...this.updateExpression(),
+      }
+    
+      componentDidUpdate({ expression }) {
+        if (this.props.expression !== expression)
+          this.updateExpression();
+      }
+    
+      updateExpression(input) {
+        const expression = input || this.props.expression || '';
+        // An expression has no side effects if it has no method calls
+        // (conservatively identified by a parenthesis)
+        const safe = !expression.includes('(');
+        const state = {
+          input: expression,
+          // Don't evaluate unsafe expressions unless the user asks
+          expression: input || safe ? expression : '',
+          // Avoid evaluating unsafe expressions a second time
+          sparql: expression && safe ? `${expression}.sparql` : '',
+        };
+        return this.state ? this.setState(state) : state;
+      }
+    
+      onChangeExpression = event => {
+        this.setState({ input: event.target.value });
+      }
+    
+      onChangeMultiple = event => {
+        this.setState({ multiple: event.target.checked });
+      }
+    
+      onExecute = event => {
+        event.preventDefault();
+        const { input } = this.state;
+        this.updateExpression(input);
+        if (this.props.onExpressionChange)
+          this.props.onExpressionChange(input);
+      }
+    
+      render() {
+        const { input, expression } = this.state;
+        return (
+          <form className="playground" onSubmit={this.onExecute}>
+            <p className="expression">
+              <label><code>solid.data</code></label>
+              <input value={input} onChange={this.onChangeExpression} required />
+              <button>Execute</button>
+            </p>
+    
+            <h3>Expression</h3>
+            <pre className="expression"><code>{expression}</code></pre>
+    
+            { <div>
+                <h3>Friends</h3>
+                <List src={expression}>{(item, i) =>
+                  <li key={i}>{linkTo(`${item}`)}</li>}
+                </List>
+              </div>
+            }
+          </form>
+        );
+      }
+    }
+    
+    function linkTo(dest) {
+      return !/^https?:/.test(dest) ? dest :
+        <a href={dest} target="_blank" rel="noopener noreferrer">{dest}</a>;
+    }
+
+
+
+ /*   render() {
+  
+      return(
+  
+        <div>
+          <h3>Friends</h3>
+          <List src={expression}>{(item, i) =>
+            <li key={i}>{linkTo(`${item}`)}</li>}
+          </List>
+        </div>
+      );
+  
+    }
+  
+  }*/
+  
+  export default SubFriendsComponent;
