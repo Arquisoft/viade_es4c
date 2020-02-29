c49c5d7cbe823cb6a69db60d939ae3e3ffa71fc7
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-02-29 14:49:23 +0100

    Fixed commentary error

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 
 import {
-  Home,Login,Friends
+  Home, Login, Friends
 } from "./containers";
 
-import {NotLoggedInLayout,PublicLayout,PrivateLayout} from "./layouts";
+import { NotLoggedInLayout, PublicLayout, PrivateLayout } from "./layouts";
 
 
 /**
@@ -14,20 +14,20 @@ import {NotLoggedInLayout,PublicLayout,PrivateLayout} from "./layouts";
  * @constructor
  */
 const Routes = () => (
-  {
-    /*
-      PublicLayout para rutas que se tienen que mostrar a usuarios que estén o no registrados
-      NotLoggedInLayout para rutas que se tienen que mostrar a usuarios que NO están registrados
-      PrivateLayout para rutas que se tienen que mostrar a usuarios que SI están registrados
-    */
-  }
   <Router>
     <Fragment>
-        {/* Chooses the first route matching the direction and loads it */}
+      {/* Chooses the first route matching the direction and loads it */}
       <Switch>
-        <PublicLayout component={Home} path="/" exact/>      {/* Homepage - "/" */}
-        <NotLoggedInLayout component={Login} path="/login" exact/> {/* Login - "/login" */}
-        <PrivateLayout component={Friends} path="/friends"  exact/> {/* Friends - "/friends" */}
+        {
+          /*
+            PublicLayout para rutas que se tienen que mostrar a todos los usuarios(estén o no loggeados)
+            NotLoggedInLayout para rutas que se tienen que mostrar a usuarios que NO están loggeados
+            PrivateLayout para rutas que se tienen que mostrar a usuarios que SI están loggeados
+          */
+        }
+        <PublicLayout component={Home} path="/" exact />      {/* Homepage - "/" */}
+        <NotLoggedInLayout component={Login} path="/login" exact /> {/* Login - "/login" */}
+        <PrivateLayout component={Friends} path="/friends" exact /> {/* Friends - "/friends" */}
         <Redirect to="/404" />      {/* If none page is loaded redirects to 404 error page */}
       </Switch>
     </Fragment>
