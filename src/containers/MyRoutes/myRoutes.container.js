f908c7d65292dc6f85648f97107acd29aadc1936
CarlosMenchacaUO258650 <UO258650@uniovi.es>
2020-03-03 20:10:25 +0100

    Uso de webID para que cabmie dinamicamente segun quien esté logeado

--- src/containers/MyRoutes/myRoutes.container.js
+++ src/containers/MyRoutes/myRoutes.container.js
@@ -1,9 +1,29 @@
 import React from "react";
+import auth from "solid-auth-cli";
+import FC from "solid-file-client";
 import { useWebId } from "@inrupt/solid-react-components";
 import {NavBar} from "../../components";
 
 const MyRoutesComponent = () => {
-  const webID = useWebId();
+  const webID = useWebId(); // https://mrmenchaca.solid.community/profile/card#me
+    
+  // I will use: https://github.com/jeff-zucker/solid-file-client
+  if (webID != null){
+    
+    const fc   = new FC( auth ) //With fc we can manage files
+    async function run(){
+
+      //Get an item which has the url with the files(routes)
+      let publicFolder = await fc.readFolder(webID.split("profile")[0] + "public") 
+
+      for(var i=0; i < publicFolder.files.length; i++){ //Iterate over the files 
+          // let file =  await fc.readHead(publicFolder.files[i].url) //Get the file's content
+          console.log(publicFolder.files[i].name) //Get the file's name
+      }
+    }
+    run()
+  }
+
   return (
       <div>
         <p>.</p>
