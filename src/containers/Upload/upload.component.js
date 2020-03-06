31109e8d0d25e2c8d53ac13e42f35f11236572ab
uo265009 <tecmanuellor1516@gmail.com>
2020-03-06 01:44:04 +0100

    upload totalmente funcional

--- src/containers/Upload/upload.component.js
+++ src/containers/Upload/upload.component.js
@@ -1,4 +1,4 @@
-import React,{Component} from  "react";
+import React,{Component,useRef, useEffect } from  "react";
 import {Link, NavLink} from 'react-router-dom';
 import { useWebId } from "@inrupt/solid-react-components";
 import {NavBar} from "../../components";
@@ -6,7 +6,8 @@ import UploadButton from "./upload.button.component";
 import SolidFileClient from "solid-file-client"
 import auth from "solid-auth-client"
 import $ from "jquery"
-
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 // const auth = require('/node_modules/solid-auth-client')
 //const FileClient = require('/node_modules/solid-file-client')
@@ -41,6 +42,11 @@ export const UploadComponent = () => {
     //setUploadStatus(false)
     //<button onClick={fileUploadToReactHandler}>UploadToReact</button>
     let files
+    let textInput = React.createRef();
+    let value  = ""
+
+
+
 
     const setUploadStatus = isUploading => {
 
@@ -63,8 +69,9 @@ export const UploadComponent = () => {
         //setUploadStatus(true)//empezamos a subir
         const file = files[0]
         //const parentContainer =
-        const parentContainer = "https://tovarashi.solid.community/public/test/"//esto esta fixeado
+        const parentContainer = value// + ((textInput.current.select().endsWith('/')) ? '' : '/')//"https://tovarashi.solid.community/public/test/"//esto esta fixeado
         const url = parentContainer + file.name
+        alert(url)
         try{
             const res = await fc.putFile(url, file, file.type)
         }catch(err){
@@ -73,7 +80,10 @@ export const UploadComponent = () => {
         //setUploadStatus(false)//terminamos de subir
     }
 
+    const handleChange =  () => {
+        value = textInput.current.value
 
+    }
 
     return (
         //<NavBar webId={webID}/>
@@ -81,6 +91,16 @@ export const UploadComponent = () => {
         //<button onClick={fileUploadHandler}>UploadToPc</button>
 
         <div className="upload" onLoad={setUploadStatus(false)}>
+            <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon1">Folder</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl ref={textInput} onChange={() => handleChange()}
+                    placeholder="https://username.solid.community/folder/"
+                    aria-label="https://username.solid.community/folder/"
+                    aria-describedby="basic-addon1"
+                />
+            </InputGroup>
             <input type="file" onChange={fileSelectedHadler}/>
             <button onClick={summitHandler}>Upload</button>
 
