dc29992d7910cd547a546fa85726c076048eb4c7
Carmen Sirgo López <uo264637@uniovi.es>
2020-03-06 17:08:23 +0100

    Some code warnings fixed

--- src/containers/Upload/upload.component.js
+++ src/containers/Upload/upload.component.js
@@ -3,33 +3,33 @@ import {Link, NavLink} from 'react-router-dom';
 import { useWebId } from "@inrupt/solid-react-components";
 import {NavBar} from "../../components";
 import UploadButton from "./upload.button.component";
-import SolidFileClient from "solid-file-client"
-import auth from "solid-auth-client"
-import $ from "jquery"
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
+import SolidFileClient from "solid-file-client";
+import auth from "solid-auth-client";
+import $ from "jquery";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 // const auth = require('/node_modules/solid-auth-client')
 //const FileClient = require('/node_modules/solid-file-client')
 //const fc = new FileClient(auth)
-const fc = new SolidFileClient(auth)
+const fc = new SolidFileClient(auth);
 
 //const local  = "file://" + process.cwd() + "/square.png"
 //const local  = "file://C:\\Users\\arvo\\WebstormProjects\\viade_es4c\\public\\tmp\\"
-const remote = "https://Tovarashi.solid.community/public/example.jpg"
-const local  = "file:///C:/Users/arvo/WebstormProjects/viade_es4c/src/containers/Upload/example.jpg"
+const remote = "https://Tovarashi.solid.community/public/example.jpg";
+const local  = "file:///C:/Users/arvo/WebstormProjects/viade_es4c/src/containers/Upload/example.jpg";
 //const local = "file://" + process.cwd() + "/example.jpg"
 
 
 
 async function fileUploadToReactHandler(){
-    let session = await auth.currentSession()
+    let session = await auth.currentSession();
     if (!session) { alert("No estas logeado") }
     else {
-        console.log(`Logged in as ${session.webId}.`)
-        alert("estas logeado")
+        console.log(`Logged in as ${session.webId}.`);
+        alert("estas logeado");
         console.log(local);
-        await fc.copyFile( local, remote )
+        await fc.copyFile( local, remote );
     }
 }
 
@@ -40,9 +40,9 @@ export const UploadComponent = () => {
     //const webid = useWebId();
     //setUploadStatus(false)
     //<button onClick={fileUploadToReactHandler}>UploadToReact</button>
-    let files
+    let files;
     let textInput = React.createRef();
-    let value  = ""
+    let value  = "";
 
 
 
@@ -50,11 +50,11 @@ export const UploadComponent = () => {
     const setUploadStatus = isUploading => {
 
         if (isUploading) {
-            $('.not-uploading').hide()
-            $('.uploading').show()
+            $(".not-uploading").hide();
+            $(".uploading").show();
         } else {
-            $('.not-uploading').show()
-            $('.uploading').hide()
+            $(".not-uploading").show();
+            $(".uploading").hide();
         }
     }
     const fileSelectedHadler= e => {
@@ -64,23 +64,23 @@ export const UploadComponent = () => {
 
 
     const summitHandler = async(e) => {
-        e.preventDefault()
+        e.preventDefault();
         //setUploadStatus(true)//empezamos a subir
-        const file = files[0]
+        const file = files[0];
         //const parentContainer =
-        const parentContainer = value// + ((textInput.current.select().endsWith('/')) ? '' : '/')//"https://tovarashi.solid.community/public/test/"//esto esta fixeado
-        const url = parentContainer + file.name
-        alert(url)
+        const parentContainer = value;// + ((textInput.current.select().endsWith('/')) ? '' : '/')//"https://tovarashi.solid.community/public/test/"//esto esta fixeado
+        const url = parentContainer + file.name;
+        alert(url);
         try{
-            const res = await fc.putFile(url, file, file.type)
+            const res = await fc.putFile(url, file, file.type);
         }catch(err){
-            console.error(err)
+            console.error(err);
         }
         //setUploadStatus(false)//terminamos de subir
     }
 
     const handleChange =  () => {
-        value = textInput.current.value
+        value = textInput.current.value;
 
     }
 
