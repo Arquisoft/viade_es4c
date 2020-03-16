6513bbdabb52182d373119ccdbeefb6daecdf03b
panchocuetodaniel <uo245313@uniovi.es>
2020-03-16 16:20:51 +0100

    V1 tests

--- src/components/ErrorBoundary/error-boundary.test.js
+++ src/components/ErrorBoundary/error-boundary.test.js
@@ -1,10 +1,13 @@
+
+// Imports
+
 /* eslint-disable no-console */
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup } from '@testing-library/react';
 import ErrorBoundary from './error-boundary.component';
+import '@testing-library/jest-dom';
 
-import 'jest-dom/extend-expect';
-
+//Arrange
 const ErrorComponent = () => {
   throw Error('Error');
 };
@@ -17,21 +20,24 @@ const shallowErrors = codeRun => {
 
   codeRun();
 
-  console.error = error;
 };
 
 afterAll(cleanup);
 
+//Act
 describe('ErrorBoundary Component', () => {
   test('caches error and display messages', () => {
     shallowErrors(() => {
+
+        // Render
       const { container } = render(
         <ErrorBoundary component={() => <h2>Error Message</h2>}>
           <ErrorComponent />
         </ErrorBoundary>
       );
 
-      expect(container).toHaveTextContent('Error Message');
+      //Assert
+      expect(container).toHaveTextContent('Something went wrong');
     });
   });
 });
