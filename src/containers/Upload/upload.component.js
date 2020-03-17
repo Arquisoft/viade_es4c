05fdabd10910498319ee4728c814a52b2308b6e2
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-03-17 20:56:18 +0100

    First partial version Parser RDF to Route Object
    - We are using a modificated version of sparql_fiddle lib.(Only sintaxs
    changes)
    - We are using SPARQL
    - We dont have de full query to get the points of the route

--- src/containers/Upload/upload.component.js
+++ src/containers/Upload/upload.component.js
@@ -3,7 +3,7 @@ import SolidFileClient from "solid-file-client";
 import auth from "solid-auth-client";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
-import {ParserToRoute,RouteToRDF,RDFToRoute} from "../../viade";
+import {ParserToRoute,RouteToRDF} from "../../viade";
 
 const fc = new SolidFileClient(auth);
 
@@ -69,7 +69,6 @@ export const UploadComponent = () => {
             </InputGroup>
             <input type="file" onChange={fileSelectedHadler}/>
             <button onClick={summitHandler}>Upload</button>
-            <button onClick={()=>{RDFToRoute.parse()}}>TEST</button>
         </div>
 
     );
--- src/viade/Parsers/ParserFromRDF/RDF-to-route.js
+++ src/viade/Parsers/ParserFromRDF/RDF-to-route.js
@@ -1,18 +1,37 @@
 import { RouteViade, ItemViade } from "../../Model";
-import data from "@solid/query-ldflex";
-
+import {sparqlFiddle} from "../../../utils";
 class RDFToRoute {
-  constructor() {}
-  parse() {
-    const ruta = data["https://christianpelaez98.solid.community/public/games/tictactoe/1581433208146.ttl#"];
+  async parse(url) {
+    const sparql =
+      `PREFIX viade:  <http://arquisoft.github.io/viadeSpec/>
+      PREFIX schema: <http://schema.org/>\
+      SELECT ?name ?description WHERE {  ?ruta a viade:Route;
+      schema:name  ?name.
+      OPTIONAL {?ruta schema:description ?description .}
+      }`;
+
+    let fiddle = {
+      data: url,
+      query: sparql,
+      wanted: "Array"
+    };
+
+    const result = await sparqlFiddle.run(fiddle).then(
+      results => {
+        return results;
+      },
+      err => console.log(err)
+    );
+    let ruta=this.arrayToRouteBasic(result);
     console.log(ruta);
-    this.prueba(ruta);
+    return ruta;
   }
 
-  prueba=async (person)=>{
-    const label = await person["http://data.totl.net/game/initialState"];
-    console.log(`\nNAME: ${label}`);
-}
+  arrayToRouteBasic=(result)=>{
+    return new RouteViade(result[0]["name"],null,result[0]["description"]);
+  }
+
+ 
 }
 
 const parser = new RDFToRoute();
