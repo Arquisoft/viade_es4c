746feeefa98c3e16a13bcb74878571dbc8fa626f
CarlosMenchacaUO258650 <UO258650@uniovi.es>
2020-03-20 00:07:39 +0100

    Read a Route from my pod with the link hardcoded

--- src/containers/MyRoutes/myRoutes.container.js
+++ src/containers/MyRoutes/myRoutes.container.js
@@ -6,50 +6,14 @@ import {Link} from "react-router-dom";
 import auth from "solid-auth-client";
 import FC from "solid-file-client";
 import {ItemViade, RouteViade} from "../../viade/Model";
+import {RDFToRoute} from "../../viade";
 
-
-// Ruta de prueba /////////////////////////////////////////////////////////
-var items = [];
-
-const item1 = new ItemViade();
-item1.longitude=42.7843378;
-item1.latitude=-8.8879561;
-item1.name="Item1";
-item1.description="Description1";
-items.push(item1);
-
-const item2 = new ItemViade();
-item2.longitude=42.7788287;
-item2.latitude=-8.8891135;
-item2.name="Item2";
-item2.description="Description2";
-items.push(item2);
-
-const item3 = new ItemViade();
-item3.longitude=42.7740152;
-item3.latitude=-8.8903451;
-item3.name="Item3";
-item3.description="Description3";
-items.push(item3);
-
-const item4 = new ItemViade();
-item4.longitude=42.7706397;
-item4.latitude=-8.8899538;
-item4.name="Item4";
-item4.description="Description4";
-items.push(item4);
-
-const exampleRoute = new RouteViade("Noia",items);
-exampleRoute.name = "Noia";
-exampleRoute.description = "Example route";
-///////////////////////////////////////////////////////////////////////////////////////
 
 class MyRoutesComponent extends React.Component {
     constructor(props) {
       super(props);
       this.state = { 
-          url: "https://mrmenchaca.solid.community/public",
-          routes: [] 
+          routes: []
         };
     }
 
@@ -59,7 +23,7 @@ class MyRoutesComponent extends React.Component {
                 <Link key={obj.name} to={{
                         pathname: "/showRoute",
                         state: {
-                            route: exampleRoute
+                            route: obj
                         }
                 }}>
                     <h3>{obj.name}</h3>
@@ -70,13 +34,23 @@ class MyRoutesComponent extends React.Component {
     async componentDidMount(){
         const fc = new FC(auth) //With fc we can manage files
 
+        // Obtengo el link de la sesion
         let session = (await auth.currentSession()).webId;
-        let sessionString = session.split("profile")[0] + "public"
-        console.log(sessionString)
+        let sessionString = session.split("profile")[0] + "public/viade"
 
+        // Obtengo los nombres de los archivos
         let folder = await fc.readFolder(sessionString)
         let array = folder.files
-        this.setState({routes : array})
+
+        // Con los nombres de los archivos, los obtengo y los parseo de RDF a Route
+        let aux = []
+        for(let i=0; i < array.length; i++){
+            let route = await RDFToRoute.parse(sessionString + "/pruebaRuta.ttl")
+            console.log(route)
+            aux.push(route)
+        }
+        console.log(aux)
+        this.setState({routes: aux})
     }
 
     render() {
--- src/viade/Parsers/ParserFromRDF/RDF-to-route.js
+++ src/viade/Parsers/ParserFromRDF/RDF-to-route.js
@@ -1,5 +1,37 @@
 import { RouteViade, ItemViade } from "../../Model";
 import {sparqlFiddle} from "../../../utils";
+
+
+var items = [];
+
+const item1 = new ItemViade();
+item1.longitude=42.7843378;
+item1.latitude=-8.8879561;
+item1.name="Item1";
+item1.description="Description1";
+items.push(item1);
+
+const item2 = new ItemViade();
+item2.longitude=42.7788287;
+item2.latitude=-8.8891135;
+item2.name="Item2";
+item2.description="Description2";
+items.push(item2);
+
+const item3 = new ItemViade();
+item3.longitude=42.7740152;
+item3.latitude=-8.8903451;
+item3.name="Item3";
+item3.description="Description3";
+items.push(item3);
+
+const item4 = new ItemViade();
+item4.longitude=42.7706397;
+item4.latitude=-8.8899538;
+item4.name="Item4";
+item4.description="Description4";
+items.push(item4);
+
 class RDFToRoute {
   async parse(url) {
     const sparql =
@@ -28,19 +60,8 @@ class RDFToRoute {
   }
 
   arrayToRouteBasic=(result)=>{
-    return new RouteViade(result[0]["name"],this.basicPoints(),result[0]["description"]);
+    return new RouteViade(result[0]["name"],items,result[0]["description"]);
   }
-
-  basicPoints=()=>{
-    let array=[];
-    let i;
-    let size=10;
-    for(i=0;i<size;i++){
-      array.push(new ItemViade(100+i,100+i,'Punto'+i));
-    }
-    return array;
-  }
-
  
 }
 
