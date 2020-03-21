14b0fd5a0b09db7c160e32b40c0f2b96102d5e64
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-03-21 13:25:35 +0100

    Parser adapted to new possible viade spec
    - We'r not taking the optional properties of the point(future fix)

--- src/viade/Parsers/ParserFromRDF/RDF-to-route.js
+++ src/viade/Parsers/ParserFromRDF/RDF-to-route.js
@@ -1,47 +1,74 @@
 import { RouteViade, ItemViade } from "../../Model";
 import {sparqlFiddle} from "../../../utils";
+
 class RDFToRoute {
-  async parse(url) {
-    const sparql =
-      `PREFIX viade:  <http://arquisoft.github.io/viadeSpec/>
-      PREFIX schema: <http://schema.org/>\
-      SELECT ?name ?description WHERE {  ?ruta a viade:Route;
-      schema:name  ?name.
-      OPTIONAL {?ruta schema:description ?description .}
-      }`;
-
-    let fiddle = {
-      data: url,
-      query: sparql,
-      wanted: "Array"
-    };
-
-    const result = await sparqlFiddle.run(fiddle).then(
-      results => {
-        return results;
-      },
-      err => console.log(err)
-    );
-    let ruta=this.arrayToRouteBasic(result);
-    console.log(ruta);
-    return ruta;
+  parse=async (urlOrContent)=> {
+    const route=await this.getRoute(urlOrContent);
+    const items=await this.getItems(urlOrContent);
+    route.items=items;
+    console.log(route);
+    return route;
   }
 
-  arrayToRouteBasic=(result)=>{
-    return new RouteViade(result[0]["name"],this.basicPoints(),result[0]["description"]);
+  getItems=async (url)=>{
+    const queryItems =
+    `PREFIX schema: <http://schema.org/>
+    PREFIX viade:<http://arquisoft.github.io/viadeSpec/>
+    PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    
+    SELECT ?lat ?long ?order ?name WHERE {
+     ?ruta a viade:Route;
+     viade:point ?point .
+     ?point schema:latitude ?lat ;
+            schema:longitude ?long ;
+            viade:order ?order.
+    }`;
+
+
+  let fiddle = {
+    data: url,
+    query: queryItems,
+    wanted: "Array"
+  };
+
+  const items = await sparqlFiddle.run(fiddle).then(
+    results => {
+      return results;
+    },
+    err => console.log(err)
+  );
+
+    return this.toItemsViade(items);
   }
 
-  basicPoints=()=>{
-    let array=[];
-    let i;
-    let size=10;
-    for(i=0;i<size;i++){
-      array.push(new ItemViade(100+i,100+i,'Punto'+i));
-    }
-    return array;
+  toItemsViade=(arrayFiddle)=>{
+    return arrayFiddle.map((obj)=>new ItemViade(obj.long,obj.lat));
   }
 
- 
+  getRoute=async(url)=>{
+    const queryRoute =
+    `PREFIX viade:  <http://arquisoft.github.io/viadeSpec/>
+    PREFIX schema: <http://schema.org/>\
+    SELECT ?name ?description WHERE {  ?ruta a viade:Route;
+    schema:name  ?name.
+    OPTIONAL {?ruta schema:description ?description .}
+    }`;
+
+  let fiddle = {
+    data: url,
+    query: queryRoute,
+    wanted: "Array"
+  };
+
+  const route = await sparqlFiddle.run(fiddle).then(
+    results => {
+      return results;
+    },
+    err => console.log(err)
+  );
+  return new RouteViade(route[0].name,null,route[0].description);
+  }
+
 }
 
 const parser = new RDFToRoute();
