08ed3e02e87e100198bf36b0452f3c9af9b198d1
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-03-21 20:19:07 +0100

    Implemented parser rdfToRoute with Comunica Engine

--- src/viade/Parsers/ParserFromRDF/RDF-to-route.js
+++ src/viade/Parsers/ParserFromRDF/RDF-to-route.js
@@ -1,34 +1,76 @@
 import { RouteViade, ItemViade } from "../../Model";
 import {sparqlFiddle} from "../../../utils";
+import * as comunica from '@comunica/actor-init-sparql';
 class RDFToRoute {
-  async parse(url) {
+
+  /**
+   * Parse route viade in RDF to a RouteViade
+   * @param {*} url URL of the route
+   * @return A promise
+   */
+  parse=async (url) =>{
+    const engine=comunica.newEngine();
     const sparql =
-      `PREFIX viade:  <http://arquisoft.github.io/viadeSpec/>
-      PREFIX schema: <http://schema.org/>\
-      SELECT ?name ?description WHERE {  ?ruta a viade:Route;
-      schema:name  ?name.
-      OPTIONAL {?ruta schema:description ?description .}
+      `PREFIX schema: <http://schema.org/>
+      PREFIX viade:<http://arquisoft.github.io/viadeSpec/>
+      PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      
+      SELECT ?lat ?long ?order ?name ?description ?name ?elevation WHERE {
+       ?route a viade:Route.
+       ?route viade:point ?point .
+       ?point schema:latitude ?lat ;
+              schema:longitude ?long ;
+              viade:order ?order.
+      OPTIONAL {?route schema:description ?description.}
+      OPTIONAL {?route schema:name ?name.}
+      OPTIONAL {?point schema:elevation ?elevation.}
       }`;
+      const result= await engine.query(sparql,{sources:[url]});
+      const { data } = await engine.resultToString(result, 'application/json');
+      return new Promise((resolve,reject)=>{
+        
+    
+    
+    let text="";
 
-    let fiddle = {
-      data: url,
-      query: sparql,
-      wanted: "Array"
-    };
+    data.on('data', (chunk) => {
+      text+=chunk;  
+    });
 
-    const result = await sparqlFiddle.run(fiddle).then(
-      results => {
-        return results;
-      },
-      err => console.log(err)
-    );
-    let ruta=this.arrayToRouteBasic(result);
-    console.log(ruta);
-    return ruta;
+    data.on('end',()=> {
+      resolve(this.getRoute(JSON.parse(text)));
+   });
+      });
+    
   }
 
-  arrayToRouteBasic=(result)=>{
-    return new RouteViade(result[0]["name"],null,result[0]["description"]);
+
+/**
+ * @param {*} results Array of JSON objects with the items value
+ * @returns RouteViade object
+ */
+  getRoute=(results)=>{
+    if(!results||!results.length) return;
+    let items=results.map((i)=>new ItemViade(this.cleanValue(i["?long"]),this.cleanValue(i["?lat"]),"","","",this.cleanValue(i["?elevation"])));
+    return new RouteViade(this.cleanValue(results[0]["?name"]),items,this.cleanValue(results[0]["?description"]));
+  }
+
+  /**
+   * Removes type of literals(RDF) and double quotes
+   *  @param {*} value value of literal
+   * 
+   * Example: "47.64458"^^http://www.w3.org/2001/XMLSchema#decimal => 47.64458
+   */
+  cleanValue=(value)=>{
+    if(!value)return;
+    return value.split("^^")[0].replace(/['"]+/g,"");
+  }
+
+  arrayToRouteBasic=async (url)=>{
+    let promise=this.parse(url);
+    let solucion=await  promise.then((result)=>result);
+    console.log(solucion);
+    //return new RouteViade(result[0]["name"],null,result[0]["description"]);
   }
 
  
@@ -36,4 +78,8 @@ class RDFToRoute {
 
 const parser = new RDFToRoute();
 
+parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta1.ttl");
+parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta2.ttl");
+parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta3.ttl");
+parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta4.ttl");
 export default parser;
