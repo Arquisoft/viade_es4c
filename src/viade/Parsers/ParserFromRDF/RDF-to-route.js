23bf13d0cc223fdd486fd4152760146fc20d26b7
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-03-21 20:29:54 +0100

    Fixed error merge

--- src/viade/Parsers/ParserFromRDF/RDF-to-route.js
+++ src/viade/Parsers/ParserFromRDF/RDF-to-route.js
@@ -1,6 +1,5 @@
 import { RouteViade, ItemViade } from "../../Model";
 import {sparqlFiddle} from "../../../utils";
-<<<<<<< HEAD
 import * as comunica from '@comunica/actor-init-sparql';
 class RDFToRoute {
 
@@ -72,88 +71,14 @@ class RDFToRoute {
     let solucion=await  promise.then((result)=>result);
     console.log(solucion);
     //return new RouteViade(result[0]["name"],null,result[0]["description"]);
-=======
-
-class RDFToRoute {
-  parse=async (urlOrContent)=> {
-    const route=await this.getRoute(urlOrContent);
-    const items=await this.getItems(urlOrContent);
-    route.items=items;
-    console.log(route);
-    return route;
-  }
-
-  getItems=async (url)=>{
-    const queryItems =
-    `PREFIX schema: <http://schema.org/>
-    PREFIX viade:<http://arquisoft.github.io/viadeSpec/>
-    PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    
-    SELECT ?lat ?long ?order ?elevation WHERE {
-     ?route a viade:Route.
-     ?route viade:point ?point .
-     ?point schema:latitude ?lat ;
-            schema:longitude ?long ;
-            viade:order ?order.
-    OPTIONAL {?point schema:elevation ?elevation.}
-    }`;
-
-
-  let fiddle = {
-    data: url,
-    query: queryItems,
-    wanted: "Array"
-  };
-
-  const items = await sparqlFiddle.run(fiddle).then(
-    results => {
-      return results;
-    },
-    err => console.log(err)
-  );
-
-    return this.toItemsViade(items);
-  }
-
-  toItemsViade=(arrayFiddle)=>{
-    return arrayFiddle.map((obj)=>new ItemViade(obj.long,obj.lat));
-  }
-
-  getRoute=async(url)=>{
-    const queryRoute =
-    `PREFIX viade:  <http://arquisoft.github.io/viadeSpec/>
-    PREFIX schema: <http://schema.org/>\
-    SELECT ?name ?description WHERE {  ?ruta a viade:Route;
-    schema:name  ?name.
-    OPTIONAL {?ruta schema:description ?description .}
-    }`;
-
-  let fiddle = {
-    data: url,
-    query: queryRoute,
-    wanted: "Array"
-  };
-
-  const route = await sparqlFiddle.run(fiddle).then(
-    results => {
-      return results;
-    },
-    err => console.log(err)
-  );
-  return new RouteViade(route[0].name,null,route[0].description);
->>>>>>> feature/parser_RDF_Route
   }
 
 }
 
 const parser = new RDFToRoute();
-<<<<<<< HEAD
 
 parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta1.ttl");
 parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta2.ttl");
 parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta3.ttl");
 parser.arrayToRouteBasic("https://christianpelaez98.solid.community/ejemplos/ruta4.ttl");
-=======
-parser.parse("https://christianpelaez98.solid.community/public/labra.ttl");
->>>>>>> feature/parser_RDF_Route
 export default parser;
