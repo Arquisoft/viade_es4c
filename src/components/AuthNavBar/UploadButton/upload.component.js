bad4c609fb27b1db1fe7d279a6f3c982f08d7ca4
uo265009 <tecmanuellor1516@gmail.com>
2020-03-27 00:45:16 +0100

    Tabuladores y espacios para codecov

--- src/components/AuthNavBar/UploadButton/upload.component.js
+++ src/components/AuthNavBar/UploadButton/upload.component.js
@@ -41,39 +41,39 @@ export const UploadComponent = () => {
 		e.preventDefault();
 		//setUploadStatus(true)//empezamos a subir
 		//let parser = new ParserToRoute(files[0]);
-    
-    if(webid) {
+
+	if(webid) {
 		const file = files[0];
 		const rutaPod = webid.substring(0, webid.length - 16) + "/public/viade/routes/";
 		const rutaMedia = webid.substring(0, webid.length - 16) + "/public/viade/media/";
 			//webid -> https://usernamme.solid.community/profile/card#me
 			const url = rutaPod + file.name.substr(0, file.name.indexOf(".")) + ".ttl";
-      //Empezamos a parsear el archivo
+	  //Empezamos a parsear el archivo
 
 		let promise = ParserToRoute.parse(file);
 		let route = await promise.then((route) => {
 			return route;
 		});
 
-      	route.name = valueName;//Valor del campo del nombre
+		route.name = valueName;//Valor del campo del nombre
 		route.description = valueDescription;//Valor del campo de descripcion
 
 		// Subida de archivos
 		try {
 			for (let i=0; i<media[0].length; i++) {
-			  	//console.log(media[0].length);
-			  	//console.log(media[0]);
-			  	await fc.putFile(rutaMedia + media[0][i].name, media[0][i], media[0][i].type);
-			  	if (media[0][i].name.includes(".mp4")){
+				//console.log(media[0].length);
+				//console.log(media[0]);
+				await fc.putFile(rutaMedia + media[0][i].name, media[0][i], media[0][i].type);
+				if (media[0][i].name.includes(".mp4")){
 					route.media.push(new VideoViade(rutaMedia,webid.substring(0, webid.length - 16),new Date()));
-			  	}
-			  	else {
+				}
+				else {
 					route.media.push(new ImageViade(rutaMedia,webid.substring(0, webid.length - 16),new Date()));
-			  	}
+				}
 
 			}
 		} catch (err) {
-			console.error(err);
+			alert("Error en la subida de archivos");//console.error(err);
 		}
 
 		let parserToRDF = new RouteToRDF(route);
@@ -83,14 +83,13 @@ export const UploadComponent = () => {
 		//Ya tenemos un String para meter en SolidFileClient
 		try {
 			//const res = await fc.putFile(url, file, file.type);
-			const res = await fc.createFile(url, strRoute, "text/turtle", {});//
-			console.log(res)
+			const res = await fc.createFile(url, strRoute, "text/turtle", {});
 		} catch (err) {
-			console.error(err); // Da warning aquí por usar la consola
+			alert("Error en la subida de archivos");//console.error(err); // Da warning aquí por usar la consola
 		}
 
-    }else {
-    	alert("Es necesario estar logeado");
+	}else {
+		alert("Es necesario estar logeado");
 	}
 			//setUploadStatus(false)//terminamos de subir
 	};
@@ -122,7 +121,7 @@ export const UploadComponent = () => {
 				withPreview={true}
 				buttonText='Choose images and videos'
 				onChange={mediaSelectedHadler}
-				imgExtension={['.jpg', '.gif', '.png', '.gif','.mp4']}
+				imgExtension={[".jpg", ".gif", ".png", ".gif",".mp4"]}
 				maxFileSize={5242880}
 			/>
 			{/** Botón de subida de archivo **/}
