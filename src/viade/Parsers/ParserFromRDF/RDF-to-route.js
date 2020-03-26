88cba016db286b2f29f9416ae68521542c0c019e
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-03-26 18:46:05 +0100

    Added order in parser RDF to route

--- src/viade/Parsers/ParserFromRDF/RDF-to-route.js
+++ src/viade/Parsers/ParserFromRDF/RDF-to-route.js
@@ -51,7 +51,7 @@ class RDFToRoute {
  */
   getRoute=(results)=>{
     if(!results||!results.length) return;
-    let items=results.map((i)=>new ItemViade(this.parseToFloat(i["?long"]),this.parseToFloat(i["?lat"]),this.parseToFloat(i["?elevation"])));
+    let items=results.map((i)=>new ItemViade(this.parseToFloat(i["?long"]),this.parseToFloat(i["?lat"]),this.parseToFloat(i["?order"]),this.parseToFloat(i["?elevation"])));
     return new RouteViade(this.cleanValue(results[0]["?name"]),items,this.cleanValue(results[0]["?description"]));
   }
   /**
