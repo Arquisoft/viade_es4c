24f096fe3678d0d06f2fd4a32cebfee1c91d9a1c
uo265009 <tecmanuellor1516@gmail.com>
2020-03-31 11:08:58 +0200

    not found codacy changes

--- src/containers/NotFound/notfound.component.js
+++ src/containers/NotFound/notfound.component.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import React from "'react';"
+import {Link, NavLink} from "react-router-dom";
 
 const NotFoundComponent = () => {
 	return (
