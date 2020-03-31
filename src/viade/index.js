d7218bc6338b992fda11caeea38fc32c2761699e
uo265009 <tecmanuellor1516@gmail.com>
2020-03-31 10:29:38 +0200

    viade\index codacy changes

--- src/viade/index.js
+++ src/viade/index.js
@@ -1,5 +1,5 @@
-import {RouteViade,ItemViade,VideoViade,ImageViade,NotificationViade} from './Model';
-import {RouteToRDF,ParserToRoute,RDFToRoute,RDFToNotification,NotificationToRDF} from './Parsers';
+import {RouteViade,ItemViade,VideoViade,ImageViade,NotificationViade} from "./Model";
+import {RouteToRDF,ParserToRoute,RDFToRoute,RDFToNotification,NotificationToRDF} from "./Parsers";
 
 export {RouteViade,ItemViade,RouteToRDF,ParserToRoute,RDFToRoute,VideoViade,ImageViade,NotificationViade,RDFToNotification,NotificationToRDF};
 
