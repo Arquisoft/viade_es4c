c5ca64162998e63aa7a659dc959d5e64e9b24550
CarlosMenchacaUO258650 <UO258650@uniovi.es>
2020-04-03 04:21:22 +0200

    Cosas que no funcionan

--- src/containers/ShowRoute/show-route.container.js
+++ src/containers/ShowRoute/show-route.container.js
@@ -7,6 +7,7 @@ import {
 } from "../../components";
 import {Col, Row} from "react-bootstrap";
 import "./show-route.css";
+import {RDFToRoute} from "../../viade";
 
 /**
  * Page which prints a Route and all its info
@@ -19,17 +20,47 @@ export class ShowRoute extends Component {
 	 */
 	constructor(props) {
 		super(props);
-		this.route = this.props.location.state.route;
+		this.linkRuta = props.location.state.route;
+		let xd = this.getRoute();
+		this.state = {
+			route: xd
+		}
+		console.log(xd)
+	}	
+	
+	
+	async componentWillMount(){
+		let promise = RDFToRoute.parse(this.linkRuta);
+		let aux = await promise.then((result)=>result);
+		this.setState({route: aux});
 	}
+	
+	
+	async getRoute(){
+		let promise = RDFToRoute.parse(this.linkRuta);
+		let aux = await promise.then((result)=>result);
+		return aux;
+	}
+
+	/*
+	static getDerivedStateFromProps(nextProps, prevState) {
+		let aux = this.getRoute();
+		return {
+		  route: aux
+		};
+	}
+	*/
+	
 
 	render() {
 		return (
 			<div>
 				<Row>
+					{console.log(this.state.route)}
 					<Col xs={12} md={2}/>
 					<Col xs={12} md={8} className="route-container">
-						<RouteTitle route={this.route}/> {/* Basic route info */}
-						<RouteMap route={this.route}/> {/* Map */}
+						<RouteTitle route={this.state.route}/> {/* Basic route info */}
+						<RouteMap route={this.state.route}/> {/* Map */}
 					</Col>
 					<Col xs={12} md={2}/>
 				</Row>
@@ -38,10 +69,10 @@ export class ShowRoute extends Component {
 					<Col xs={12} md={6}>
 						{/* Images of the route */}
 						<div className="image-slide"><RoutesCarousel/></div>
-						{(this.route.media.length !== 0) ? <div className="image-slide"><RoutesCarousel/></div> : null}
+						{(this.state.route.media.length !== 0) ? <div className="image-slide"><RoutesCarousel/></div> : null}
 					</Col>
 					<Col xs={12} md={2}>
-						<RoutesItinerary route={this.route}/> {/* List of points of the route */}
+						<RoutesItinerary route={this.state.route}/> {/* List of points of the route */}
 					</Col>
 					<Col xs={12} md={2}/>
 				</Row>
