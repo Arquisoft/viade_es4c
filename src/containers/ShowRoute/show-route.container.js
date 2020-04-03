e179276daf3bb867d688d727168a3c105e3f85fc
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-04-03 16:31:22 +0200

    ShowRoute now loads asynchronously

--- src/containers/ShowRoute/show-route.container.js
+++ src/containers/ShowRoute/show-route.container.js
@@ -1,10 +1,5 @@
 import React, {Component} from "react";
-import {
-	RouteMap,
-	RoutesCarousel,
-	RoutesItinerary,
-	RouteTitle,
-} from "../../components";
+import {RouteMap, RoutesCarousel, RoutesItinerary, RouteTitle,} from "../../components";
 import {Col, Row} from "react-bootstrap";
 import "./show-route.css";
 import {RDFToRoute} from "../../viade";
@@ -21,46 +16,27 @@ export class ShowRoute extends Component {
 	constructor(props) {
 		super(props);
 		this.linkRuta = props.location.state.route;
-		let xd = this.getRoute();
-		this.state = {
-			route: xd
-		}
-		console.log(xd)
-	}	
-	
-	
-	async componentWillMount(){
-		let promise = RDFToRoute.parse(this.linkRuta);
-		let aux = await promise.then((result)=>result);
-		this.setState({route: aux});
+		this.state = {promiseIsResolved: false};
+		console.log(this.state);
+		this.route = this.getRoute();
 	}
-	
 	
 	async getRoute(){
 		let promise = RDFToRoute.parse(this.linkRuta);
-		let aux = await promise.then((result)=>result);
-		return aux;
+		return await promise.then((res) => {
+			this.route = res;
+			this.setState({promiseIsResolved: true})
+		})
 	}
-
-	/*
-	static getDerivedStateFromProps(nextProps, prevState) {
-		let aux = this.getRoute();
-		return {
-		  route: aux
-		};
-	}
-	*/
-	
 
 	render() {
 		return (
-			<div>
+			(this.state.promiseIsResolved) ? <div>
 				<Row>
-					{console.log(this.state.route)}
 					<Col xs={12} md={2}/>
 					<Col xs={12} md={8} className="route-container">
-						<RouteTitle route={this.state.route}/> {/* Basic route info */}
-						<RouteMap route={this.state.route}/> {/* Map */}
+						<RouteTitle route={this.route}/> {/* Basic route info */}
+						<RouteMap route={this.route}/> {/* Map */}
 					</Col>
 					<Col xs={12} md={2}/>
 				</Row>
@@ -68,15 +44,14 @@ export class ShowRoute extends Component {
 					<Col xs={12} md={2}/>
 					<Col xs={12} md={6}>
 						{/* Images of the route */}
-						<div className="image-slide"><RoutesCarousel/></div>
-						{(this.state.route.media.length !== 0) ? <div className="image-slide"><RoutesCarousel/></div> : null}
+						{(this.route.media.length !== 0) ? <div className="image-slide"><RoutesCarousel/></div> : null}
 					</Col>
 					<Col xs={12} md={2}>
-						<RoutesItinerary route={this.state.route}/> {/* List of points of the route */}
+						<RoutesItinerary route={this.route}/> {/* List of points of the route */}
 					</Col>
 					<Col xs={12} md={2}/>
 				</Row>
-			</div>
+			</div> : <h1 style={{textAlign: "center"}}>CÓMEME ESTA LABRAAAA</h1>
 		);
 	}
 }
