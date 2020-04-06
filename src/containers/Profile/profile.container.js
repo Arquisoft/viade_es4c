0604321b6256c9acd08ffd121407bdd9dcb1fc07
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-04-06 23:05:35 +0200

    Added functions to complete profile
    - Function to get num of routes of the user
    - Function to get num of routes that other users had shared with the
    user

--- src/containers/Profile/profile.container.js
+++ src/containers/Profile/profile.container.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {List, Value, withWebId, Image, LoggedIn} from "@solid/react";
 import {ProfileCard} from "../../components";
+import {routeHelper} from "../../viade";
 import Container from "react-bootstrap/Container";
 import "./profile.container.css";
 
 class ProfileComponent extends React.Component {
+
+    getNumMyRoutes=async ()=>{
+        let myRoutes=await routeHelper.fetchUrlMyRoutes();
+        return myRoutes.length();
+    }
+
+    getNumSharedWithMe=async()=>{
+        let sharedWithMe=await routeHelper.fetchUrlSharedWithMeRoutes();
+        return sharedWithMe.length();   
+    }
 
     render() {
         return (
@@ -14,9 +25,10 @@ class ProfileComponent extends React.Component {
                         image={<Image src="user.image" defaultSrc={process.env.PUBLIC_URL + "/img/cards/profile.png"}
                                       className="profile route-card-image"  alt={"Profile image"}/>}
                         name={<Value src="user.name"/>}
-                        user={<Value src="user"/>}/>
-
-
+                        user={<Value src="user"/>}
+                        getNumMyRoutes={this.getNumMyRoutes}
+                        getNumSharedWithMe={this.getNumSharedWithMe}
+                        />
 
                     <h2>Friends</h2>
                     <List src="user.friends">
