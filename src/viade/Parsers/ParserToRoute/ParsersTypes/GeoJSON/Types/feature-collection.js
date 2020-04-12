3cd00370a94b2acbe03600eb29df77e4180cb240
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-04-13 01:12:25 +0200

    more codacy

--- src/viade/Parsers/ParserToRoute/ParsersTypes/GeoJSON/Types/feature-collection.js
+++ src/viade/Parsers/ParserToRoute/ParsersTypes/GeoJSON/Types/feature-collection.js
@@ -2,9 +2,9 @@ import Feature from "./feature";
 
 class FeatureCollection{
     
-    execute=(geoJSON)=> {
+    execute=(geoJSON)=>  {
         const features=geoJSON.features;
-        if(features.length>1){throw new Error("FeatureCollection of route GeoJSON not supported")};
+        if(features.length>1){throw new Error("FeatureCollection of route GeoJSON not supported");}
         this.parser=new Feature();
         return this.parser.execute(features[0]);
     }
