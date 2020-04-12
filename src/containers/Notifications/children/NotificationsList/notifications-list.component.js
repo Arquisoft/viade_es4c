cdfe0a7cdd294a7fd6d8f2dd1c1c5d46c7b1f41b
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-04-12 18:39:50 +0200

    Implemented Loader on Notifications and the Routes pages

--- src/containers/Notifications/children/NotificationsList/notifications-list.component.js
+++ src/containers/Notifications/children/NotificationsList/notifications-list.component.js
@@ -2,7 +2,9 @@ import React, {useState} from "react";
 import {notificationHelper} from "../../../../viade";
 import NotificationItem from "../NotificationItem";
 import {Col, Row} from "react-bootstrap";
-import { errorToaster } from "../../../../utils";
+import {errorToaster} from "../../../../utils";
+import {Loader} from "../../../../components";
+import Spacer from "../../../../components/Spacer";
 
 const NotificationsList = (props) => {
 	const {inboxes} = props;
@@ -18,19 +20,19 @@ const NotificationsList = (props) => {
 			setNotifications(urls);
 		} catch (error) {
 			console.error(error);
-			errorToaster(error.message,error.name);
+			errorToaster(error.message, error.name);
 		}
 	};
 	initNotifications();
 
 	return (
 		<div>
-			<Row className="justify-content-md-center">
-				<Col key="col-1" xs={12} sm={12} md={12} lg={1} xl={1}/>
-				<Col key="col-2" xs={12} sm={12} md={12} lg={10} xl={10}>
-					<Row>
-						{notifications
-							? notifications.length > 0
+			{notifications
+				? <Row className="justify-content-md-center">
+					<Col key="col-1" xs={12} sm={12} md={12} lg={1} xl={1}/>
+					<Col key="col-2" xs={12} sm={12} md={12} lg={10} xl={10}>
+						<Row>
+							{notifications.length > 0
 								? notifications.map((url, index) => (
 									<Col md={12} lg={4} key={index}>
 										<NotificationItem
@@ -40,18 +42,17 @@ const NotificationsList = (props) => {
 											url={url}
 											{...props}
 										/>
-									</Col>
-								))
+									</Col>))
 								: <h4 style={{textAlign: "center", margin: "15px 0 0 0", width: "100%"}}>
 									Sadly, you don't have notifications to show here yet :(
-								</h4>
-							: <p>Colocar Loader Pendiente</p> }
-					</Row>
-				</Col>
-				<Col key="col-3" xs={12} sm={12} md={12} lg={1} xl={1}/>
-			</Row>
+								</h4>}
+						</Row>
+					</Col>
+					<Col key="col-3" xs={12} sm={12} md={12} lg={1} xl={1}/>
+				</Row>
+				: <div><Spacer/><Loader/></div>}
 		</div>
-	);
+	)
 };
 
 export default NotificationsList;
--- src/containers/Notifications/notifications.component.js
+++ src/containers/Notifications/notifications.component.js
@@ -1,57 +1,56 @@
-import React,{useState,useCallback,useEffect} from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import {NotificationsList} from "./children";
 import {ldflexHelper, errorToaster} from "../.././utils";
-import {RDFToRoute} from "../.././viade";
 import auth from "solid-auth-client";
 import FC from "solid-file-client";
+
 const fc = new FC(auth);
 
 const Notifications = (props) => {
-    const [inboxes, setInbox] = useState([]);
-    const [image,setImage]=useState([]);
-    const { webId } = props;
-    
-    const discoverInbox = useCallback(async () => {
-        try {
-          let inboxes = [];
-          /**
-           * Get user's global inbox path from pod.
-           */
-          const globalInbox = await ldflexHelper.discoverInbox(webId);
-    
-          if (globalInbox) {
-            inboxes = [
-              ...inboxes,
-              { path: globalInbox, inboxName: "Global", shape: "default" }
-            ];
-          }
-          /**
-           * If user doesn't has inbox in his pod will show an error and link to
-           * know how fix it.
-           */
-          if (inboxes.length === 0) {
-            errorToaster("Your inbox couldn't be found","Error");
-          }
-          setInbox(inboxes);
-        } catch (error) {
-          console.error(error);
-          errorToaster("An error has occurred with the inbox folder","Error");
-        }
-      }, [webId]);
+	const [inboxes, setInbox] = useState([]);
+	const [image, setImage] = useState([]);
+	const {webId} = props;
 
-      const inboxUrl = inboxes.map((item) => item.path);
+	const discoverInbox = useCallback(async () => {
+		try {
+			let inboxes = [];
+			/**
+			 * Get user's global inbox path from pod.
+			 */
+			const globalInbox = await ldflexHelper.discoverInbox(webId);
 
-      useEffect(() => {
-        if (webId) {
-          discoverInbox();
-        }
-      }, [webId,discoverInbox]);
+			if (globalInbox) {
+				inboxes = [
+					...inboxes,
+					{path: globalInbox, inboxName: "Global", shape: "default"}
+				];
+			}
+			/**
+			 * If user doesn't has inbox in his pod will show an error and link to
+			 * know how fix it.
+			 */
+			if (inboxes.length === 0) {
+				errorToaster("Your inbox couldn't be found", "Error");
+			}
+			setInbox(inboxes);
+		} catch (error) {
+			errorToaster("An error has occurred with the inbox folder", "Error");
+		}
+	}, [webId]);
 
-  return (
-    <div>
-      <NotificationsList inboxes={inboxUrl} {...props}/>
-      </div>
-  );
+	const inboxUrl = inboxes.map((item) => item.path);
+
+	useEffect(() => {
+		if (webId) {
+			discoverInbox();
+		}
+	}, [webId, discoverInbox]);
+
+	return (
+		<div>
+			<NotificationsList inboxes={inboxUrl} {...props}/>
+		</div>
+	);
 };
 
 export default Notifications;
