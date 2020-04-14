9482f008cc9c63dc29e5b7db06e3ff7992461c23
Carmen Sirgo López <uo264637@uniovi.es>
2020-04-14 19:39:23 +0200

    More codacy issues fixed

--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { toast } from 'react-toastify';
-import { Toaster } from '../components';
+import React from "react";
+import { toast } from "react-toastify";
+import { Toaster } from "../components";
 
 export const errorToaster = (content: String, title: String = null, link: Object) =>
-  toast(<Toaster {...{ content, title, type: 'error', link }} />, {
+  toast(<Toaster {...{ content, title, type: "error", link }} />, {
     autoClose: false,
-    className: 'toaster-error',
-    type: 'error'
+    className: "toaster-error",
+    type: "error"
   });
 
 export const successToaster = (content: String, title: String = null, link: Object) =>
-  toast(<Toaster {...{ content, title, type: 'success', link }} />, {
-    className: 'toaster-success',
-    type: 'success'
+  toast(<Toaster {...{ content, title, type: "success", link }} />, {
+    className: "toaster-success",
+    type: "success"
   });
