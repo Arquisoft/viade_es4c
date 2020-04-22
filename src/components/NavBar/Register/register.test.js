26a18c6fd4939bb3b5a1e117ee34e11ea2b0b073
CarlosMenchacaUO258650 <UO258650@uniovi.es>
2020-04-22 13:04:15 +0200

    Añadidos varios test

---
+++ src/components/NavBar/Register/register.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {cleanup, render} from 'react-testing-library';
+import RegisterContainer from './index';
+
+describe.only('RegisterContainer', () => {
+  afterAll(cleanup);
+
+  const { container } = render(
+      <RegisterContainer/>
+  );
+
+  test('renders without crashing', () => {
+    expect(container).toBeTruthy();
+  });
+});
