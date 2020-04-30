b101e84191a9bff10d4063dbd15a64542479e105
Christian Peláez Fernández <christianpe98@users.noreply.github.com>
2020-04-30 22:07:46 +0200

    Added information unsupported route

--- src/viade/util/route-helper.js
+++ src/viade/util/route-helper.js
@@ -3,7 +3,17 @@ import { storageHelper, Fetcher } from "../util";
 import auth from "solid-auth-client";
 import FC from "solid-file-client";
 import sparql from "../sparql-queries.json";
+import { infoToaster } from "../../utils";
 const fc = new FC(auth);
+
+const supportRoutes=(array)=>{
+  let supported=array.filter((url)=>url.split(".")[1]==="ttl");
+  console.log(supported);
+  if(array.length!==supported.length){
+    infoToaster("There are routes on your POD that are not supported in our app :(");
+  }
+  return supported;
+}
 
 export const fetchUrlSharedWithMeRoutes = async () => {
   try {
@@ -27,7 +37,9 @@ export const fetchUrlMyRoutes = async () => {
       return [];
     }
     let routes = await fc.readFolder(folder);
-    return routes.files.map((file) => file.url);
+    let array= routes.files.map((file) => file.url);
+    supportRoutes(array);
+    return array;
   } catch (err) {
     console.error(err);
     throw new Error("An error has occurred loading your routes");
