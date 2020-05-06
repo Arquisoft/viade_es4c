3ec1c10aad5db4e92423dfa1dff5d7045d6b504e
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-05-06 14:17:16 +0200

    RouteMap tests

---
+++ src/components/RoutesMap/RouteMap/routes-map.test.js
@@ -0,0 +1,26 @@
+import {cleanup, render} from "react-testing-library";
+import React from "react";
+import RouteMap from "./index";
+import {ItemViade, RouteViade} from "../../../viade";
+
+describe.only("RouteMapComponent", () => {
+	afterAll(cleanup);
+
+	jest.mock('leaflet');
+
+	const mockRoute = new RouteViade("Test", [
+		new ItemViade(1.0, 1.0, 1),
+		new ItemViade(1.1, 1.1, 2),
+		new ItemViade(1.2, 1.2, 3),
+		new ItemViade(1.3, 1.3, 4)
+	]);
+
+	const {container} = render(
+		<RouteMap route={mockRoute}/>
+	);
+
+	test("renders without crashing", () => {
+		expect(container).toBeTruthy();
+	});
+
+});
